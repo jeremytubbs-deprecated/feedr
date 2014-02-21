67db9fa7295ec2f663f399719ab5f5ae65ce7a15
Collin Schneider <collin@thinksaydo.com>
2014-02-20 21:02:49 -0500

    Initial commit

---
+++ src/FeedrServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace IndyLaravel\Feedr;
+
+use Illuminate\Support\ServiceProvider;
+
+class FeedrServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('indy-laravel/feedr');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
